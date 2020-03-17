db486dcf962f97e3435367fe988ac9e5ea5aee63
EvelinaTjahjadi <62284716+EvelinaTjahjadi@users.noreply.github.com>
2020-03-17 16:49:41 +0700

    Add files via upload

---
+++ go.php
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+         <!--
+            function Redirect() {
+               window.location = "https://google.com";
+            }            
+            document.write("You will be redirected to main page in 3 sec.");
+            setTimeout('Redirect()', 2000);
+         //-->
+      </script>
